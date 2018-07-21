930ef990ced0aed1ade8bd3ec2f60a0f159dd686
Zoe Ames <zoeames@gmail.com>
2018-07-21 14:46:17 -0500

    added yesterdays commits

--- server.js
+++ server.js
@@ -25,7 +25,21 @@ app.get('/get_github_user/:username', (req, res) => {
   .catch(error => {
     console.log(error);
   });
-})
+});
+
+app.get('/todays_commits/:username', (req, res) => {
+  console.log('req', );
+  const username = req.params.username;
+  const clientId = process.env.CLIENT_ID;
+  const clientSecret = process.env.CLIENT_SECRET;
+  axios.get(`https://api.github.com/users/${username}/events?client_id=${clientId}&client_secret=${clientSecret}`)
+  .then(response => {
+    res.json(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+});
 
 //// You know, like, listen on the port or something something darkside
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
